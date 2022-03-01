bd81282cbb3ea2b5ee23d0b9a7e8291abb9f5497
James Greer <88616420+James-Greer-Git@users.noreply.github.com>
2022-03-01 10:46:58 +0000

    Update collatzCalculator.java

--- Collatz Length/collatzCalculator.java
+++ Collatz Length/collatzCalculator.java
@@ -6,27 +6,31 @@ import java.util.Scanner;
 public class collatzCalculator{
     public static void main(String args[]){
         Scanner scanner = new Scanner(System.in);
-        int min = scanner.nextInt();
-        int max = scanner.nextInt();
+        long min = scanner.nextLong();
+        long max = scanner.nextLong();
         int index = scanner.nextInt();
         scanner.close();
 
-        HashMap<Integer, Integer> unsortedMap = collatzLengthFromRange(min, max);
-        System.out.println(getElement(unsortedMap, min, max, index));
-    }
-    public static HashMap<Integer, Integer> collatzLengthFromRange(int min, int max){
-        HashMap<Integer, Integer> collatzLengthMap = new HashMap<Integer,Integer>();
+        List<Long> values = new ArrayList<>();
+        List<Long> lengths = new ArrayList<>();
+        //HashMap<Long, Long> lengthMap = new HashMap<>();
+
+        for(long i = min; i <= max; i++){
+            values.add(i);
+            lengths.add(collatzLength(i));
+            //lengthMap.put(i, collatzLength(i));
+        }
+        if(index == 0){
+            index++;
+        }
+        sort(values, lengths, 0, lengths.size() - 1);
+        sortByValue(values, lengths);
         
-        for(int i = min; i <= max; i++){
-            collatzLengthMap.put(i, collatzLength(i));
-        }
-        return collatzLengthMap;
+        System.out.println(values.get(index - 1));
     }
-    public static int collatzLength(int element){
-        if(element == 0 || element == 1){
-            return 0;
-        }
-        int length = 0;
+    
+    public static Long collatzLength(Long element){
+        Long length = (long) 0;
 
         while(element != 1){
             if(element%2 == 0){     //if element is even
@@ -37,36 +41,54 @@ public class collatzCalculator{
             }
             length++;
         }
-
         return length;
     }
-    public static int getElement(HashMap<Integer, Integer> unsortedMap, int min, int max, int index){
-        if((max - min) <= index){
-            return 0;
+    static void sortByValue(List<Long> values, List<Long> lengths){
+        int tempIndex = 0;
+        long tempVal = values.get(0);
+        for(int i = 0; i < values.size(); i++){
+            if(values.get(i) != tempVal){
+                //sort between tempVal and i - 1
+                valueSort(values, tempIndex, i - 1);
+            }
+            tempIndex = i;
+            tempVal = values.get(i);   
         }
-            
-        List<Integer> keys = new ArrayList<>();
-        List<Integer> values = new ArrayList<>();
-
-        for(int i = min; i <= max; i++){
-            keys.add(i);
-            values.add(unsortedMap.get(i));
-        }
-        sort(keys, values, 0, values.size() - 1);
-        
-        
-        return keys.get(index - 1);
     }
-    static void swap(List<Integer> keys, List<Integer> values, int i, int j){
-        int tempVal = values.get(i);
-        int tempKey = keys.get(i);
+    static void valueSort(List<Long> values, int low, int high){
+        if(low < high){
+            int p = valuePartition(values, low, high);
+            valueSort(values, low, p - 1);
+            valueSort(values, p + 1, high);
+        }
+    }
+    static int valuePartition(List<Long> values, int low, int high){
+        Long pivot = values.get(high);
+        int i = (low - 1);
+        for(int j = low; j <= high - 1; j++){
+            if(values.get(j) < pivot){
+                i++;
+                valueSwap(values, i, j);
+            }
+        }
+        valueSwap(values, i + 1, high);
+        return i + 1;
+    }
+    static void valueSwap(List<Long> values, int i, int j){
+        Long tempVal = values.get(i);
+        values.set(i, values.get(j));
+        values.set(j, tempVal);
+    }
+    static void swap(List<Long> keys, List<Long> values, int i, int j){
+        Long tempVal = values.get(i);
+        Long tempKey = keys.get(i);
         values.set(i, values.get(j));
         keys.set(i, keys.get(j));
         values.set(j, tempVal);
         keys.set(j, tempKey);
     }
-    static int partition(List<Integer> keys, List<Integer> values, int low, int high){
-        int pivot = values.get(high);
+    static int partition(List<Long> keys, List<Long> values, int low, int high){
+        Long pivot = values.get(high);
         int i = (low - 1);
         for(int j = low; j <= high - 1; j++){
             if(values.get(j) < pivot){
@@ -77,7 +99,7 @@ public class collatzCalculator{
         swap(keys, values, i + 1, high);
         return i + 1;
     }
-    static void sort(List<Integer> keys, List<Integer> values, int low, int high){
+    static void sort(List<Long> keys, List<Long> values, int low, int high){
         if(low < high){
             int p = partition(keys, values, low, high);
             sort(keys, values, low, p - 1);
